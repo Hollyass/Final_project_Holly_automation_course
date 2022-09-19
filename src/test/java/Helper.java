feb46c17681133c88c882d105a52049de52bcbea
Holly <holly.shamraevsky@goglobal.travel>
2022-09-19 22:00:47 +0300

    alert test + updates to helper

--- src/test/java/Helper.java
+++ src/test/java/Helper.java
@@ -1,3 +1,42 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Helper {
+    //file paths
+    public static String FILEPATH = "C:\\Users\\HollyShamraevsky\\Desktop\\FinalProject\\";
+    public static String JPG = ".jpg";
+    public static String TXT = ".txt";
+
+
+    // Test alerts
+    public static String ALERATURL = "https://hollyass.github.io/alertsfortests/";
+    public static String ALERTID = "btnAlert";
+    public static String PROMPTID = "btnPrompt";
+    public static String AFTERPROMPTTEXT = "afterPrompt";
+
+
+
+    public static ChromeDriver setupDriver (){
+        System.setProperty("webdriver.chrome.driver","res\\chromedriver.exe");
+        return new ChromeDriver();
+    }
+
+    public static void createFile(String pathname,String filename, String text) throws IOException {
+        File file = new File(pathname + filename);
+        if (file.createNewFile()){
+            System.out.println("File was created successfully");
+        }else {
+            System.out.println("File was previously created");
+        }
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
+    }
+
 
 }
+
--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -1,3 +1,39 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class ProjectTest {
 
+    @Test
+    void navigate (){
+        WebDriver driver = Helper.setupDriver();
+        driver.get("");
+
+     //   File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //FileUtils.copyFile(screenshot,new File(Helper.FILEPATH +"Alert shown"+Helper.JPG));
+    }
+
+    @Test
+    void alerts() throws IOException {
+        WebDriver driver = Helper.setupDriver();
+        driver.get(Helper.ALERATURL);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.ALERTID))).click();
+        wait.until(ExpectedConditions.alertIsPresent()).accept();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.PROMPTID))).click();
+        wait.until(ExpectedConditions.alertIsPresent()).sendKeys("Debbie");
+        driver.switchTo().alert().accept();
+
+        String acceptedText = driver.findElement(By.id(Helper.AFTERPROMPTTEXT)).getText();
+        Helper.createFile(Helper.FILEPATH,"Prompt alert message"+Helper.TXT,acceptedText);
+
+    }
 }
